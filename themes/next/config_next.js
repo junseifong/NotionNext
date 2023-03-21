7d33d251d91c8d946a4df237ad11d8fa3bb08a14
junseifong <56868572+junseifong@users.noreply.github.com>
2023-03-21 15:11:38 +0800

    Update config_next.js

--- themes/next/config_next.js
+++ themes/next/config_next.js
@@ -12,18 +12,18 @@ const CONFIG_NEXT = {
   POST_HEADER_IMAGE_VISIBLE: false, // 文章详情页是否显示封面图
 
   // 右侧组件
-  RIGHT_BAR: true, // 是否显示右侧栏
-  RIGHT_LATEST_POSTS: true, // 右侧栏最新文章
-  RIGHT_CATEGORY_LIST: true, // 右侧边栏文章分类列表
-  RIGHT_TAG_LIST: true, // 右侧边栏标签分类列表
+  RIGHT_BAR: false, // 是否显示右侧栏
+  RIGHT_LATEST_POSTS: false, // 右侧栏最新文章
+  RIGHT_CATEGORY_LIST: false, // 右侧边栏文章分类列表
+  RIGHT_TAG_LIST: false, // 右侧边栏标签分类列表
   RIGHT_AD: false, // 右侧广告
 
   // 菜单
   MENU_HOME: true, // 显示首页
   MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
-  MENU_SEARCH: true, // 显示搜索
+  MENU_TAG: false, // 显示标签
+  MENU_ARCHIVE: false, // 显示归档
+  MENU_SEARCH: false, // 显示搜索
 
   WIDGET_TO_TOP: true, // 是否显示回顶
   WIDGET_TO_BOTTOM: false, // 显示回底
@@ -31,8 +31,8 @@ const CONFIG_NEXT = {
   WIDGET_TOC: true, // 移动端显示悬浮目录
 
   ARTICLE_SHARE: process.env.NEXT_PUBLIC_ARTICLE_SHARE || false, // 文章分享功能
-  ARTICLE_RELATE_POSTS: true, // 相关文章推荐
-  ARTICLE_COPYRIGHT: true // 文章版权声明
+  ARTICLE_RELATE_POSTS: false, // 相关文章推荐
+  ARTICLE_COPYRIGHT: false // 文章版权声明
 
 }
 
